936d8b47931c140ae2ff4dcb1fc17b67d4a4601f
Ohdong Kwon <dhehd9709@gmail.com>
2024-05-31 15:44:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-
-
-var randomNumber1 = Math.floor(Math.random()*6)+1;
-var imgSrc1 = document.querySelector(".img1");//document.querySelectorAll("img")[0]
-var randomImg1 = "images/dice"+randomNumber1+".png"
-imgSrc1.setAttribute("src",randomImg1);
-
-var randomNumber2 = Math.floor(Math.random()*6)+1;
-var imgSrc2 = document.querySelector(".img2");//document.querySelectorAll("img")[1]
-var randomImg2 = "images/dice"+randomNumber2+".png"
-imgSrc2.setAttribute("src",randomImg2);
-
-
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩May Won!";
-}
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Will Won!🚩";
-}
+function play() {
+var randomNum1 = Math.floor(Math.random()*6+1);
+document.querySelector(".img1").setAttribute("src", "./images/dice"+randomNum1+".png");
+var randomNum2 = Math.floor(Math.random()*6+1);
+document.querySelector(".img2").setAttribute("src", "./images/dice"+randomNum2+".png");
+if (randomNum1>randomNum2) {
+    document.querySelector("h1").innerHTML =
+    "🚩 May Wins"
+} 
+else if (randomNum1<randomNum2) {
+    document.querySelector("h1").innerHTML =
+    "William Wins🚩"
+} 
 else {
-  document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML =
+    "Draw!"
+
 }
+}
+function reset() {
+    
+    document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
+    var randomNum2 = Math.floor(Math.random()*6+1);
+    document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
+    
+    document.querySelector("h1").innerHTML ="Refresh me";    
+   
+    }
+    
